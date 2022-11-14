1b63fc6c49e3087f21421d505a853811bc69783b
voyagerdva <mariner-9@yandex.ru>
2022-11-14 19:50:37 +0300

    Initial commit

---
+++ src/main/java/ru/zhuravl/LinkedList.java
@@ -0,0 +1,10 @@
+package ru.zhuravl;
+
+public class LinkedList {
+    void createNewLinkedList() {
+        System.out.println("--------------");
+        System.out.println("From LinkedList class - we created LL !!!");
+        System.out.println("--------------");
+    }
+
+}
